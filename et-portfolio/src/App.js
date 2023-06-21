e954bbfd6f9006d38d64281ecadc89b9b7c287b3
Elizabeth Tutor <elitutor0116@gmail.com>
2023-06-21 16:25:21 -0700

    Added route paths to app.js

--- et-portfolio/src/App.js
+++ et-portfolio/src/App.js
@@ -1,5 +1,7 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
 import "./App.css";
+import Navigation from "./Navigation";
 import Home from "./Home";
 import Hero from "./Hero";
 import AboutMe from "./AboutMe";
@@ -8,14 +10,17 @@ import Projects from "./Projects";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <AboutMe />
-      <Hero />
-      <Contacts />
-      <Projects />
-      {/* Eventually we want to use routing / client side routing */}
-    </div>
+    <Router>
+      <Navigation />
+      <Switch>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route path="about-me" component={AboutMe} />
+          <Route path="contacts" component={Contacts} />
+          <Route path="projects" component={Projects} />
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
